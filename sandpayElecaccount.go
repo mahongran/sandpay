3d1add29564e0c1f406335ab9a06852781c1fce2
mahongran <623893204@qq.com>
2023-02-21 15:45:02 +0800

    云账户一键开户

---
+++ sandpayElecaccount.go
@@ -0,0 +1,70 @@
+package sandpay
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mahongran/sandpay/pay"
+	"github.com/mahongran/sandpay/pay/elecaccountParams"
+	"github.com/mahongran/sandpay/pay/elecaccountRequest"
+	"github.com/mahongran/sandpay/util"
+	"time"
+)
+
+// OneClickAccountOpening 云账户一键开户
+func (sandPay *SandPay) OneClickAccountOpening(params elecaccountParams.OneClickAccountOpening) (string, error) {
+	config := sandPay.Config
+	timeString := time.Now().Format("20060102150405")
+	body := elecaccountRequest.OneClickAccountOpening{
+		Mid:             config.MerId,
+		Version:         "1.0",
+		SignType:        "SHA1WithRSA",
+		EncryptType:     "AES",
+		Timestamp:       timeString,
+		CustomerOrderNo: params.CustomerOrderNo,
+		BizUserNo:       params.BizUserNo,
+		NickName:        params.NickName,
+		Name:            params.Name,
+		IdType:          params.IdType,
+		Mobile:          params.Mobile,
+		NotifyUrl:       sandPay.Config.NotifyUrl,
+		FrontUrl:        sandPay.Config.FrontUrl,
+	}
+	sanDe := util.SandAES{}
+	key := sanDe.RandStr(16)
+	postData, _ := FormData(body, key)
+	body.EncryptType, _ = pay.FormEncryptKey(key)
+	body.Sign, _ = pay.FormSign(postData)
+	body.Data = postData
+	//display(fd)
+
+	DataByte, _ := json.Marshal(body)
+	resp, err := util.Do(config.CloudAccountApiHost+"/v4/elecaccount/ceas.elec.account.protocol.open", string(DataByte))
+	if err != nil {
+		return "", err
+	}
+	d := make(map[string]interface{})
+	if err := json.Unmarshal(resp, &d); err != nil {
+		return "", err
+	}
+	fmt.Println("杉德回调解析结果:" + string(resp))
+	return string(resp), nil
+}
+
+func display(fd map[string]interface{}) {
+	for k, v := range fd {
+		fmt.Println(k, "->", v)
+	}
+}
+
+func FormData(paraMap interface{}, key string) (string, error) {
+
+	dataJson, err := json.Marshal(paraMap)
+	if err != nil {
+		return "", err
+	}
+	aes := util.SandAES{}
+	aes.Key = []byte(key)
+
+	data := aes.Encypt5(dataJson)
+	return data, nil
+}
