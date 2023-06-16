d22f615e68d9bbed39a4f8ae2f140eaa70c973a0
mahongran <623893204@qq.com>
2023-06-16 11:46:24 +0800

    fix

--- pay/response/order_pay.go
+++ pay/response/order_pay.go
@@ -5,16 +5,18 @@ import "encoding/json"
 type OrderPayResponse struct {
 	Head header `json:"head"`
 	Body struct {
-		TotalAmount    string `json:"totalAmount"`
-		ClearDate      string `json:"clearDate"`
-		Credential     string `json:"credential"`
-		TradeNo        string `json:"tradeNo"`
-		PayTime        string `json:"payTime"`
-		BuyerPayAmount string `json:"buyerPayAmount"`
-		OrderCode      string `json:"orderCode"`
-		QrCode		   string	`json:"qrCode"`
-		DiscAmount     string `json:"discAmount"`
+		TotalAmount       string `json:"totalAmount"`
+		ClearDate         string `json:"clearDate"`
+		Credential        string `json:"credential"`
+		TradeNo           string `json:"tradeNo"`
+		PayTime           string `json:"payTime"`
+		BuyerPayAmount    string `json:"buyerPayAmount"`
+		OrderCode         string `json:"orderCode"`
+		QrCode            string `json:"qrCode"`
+		DiscAmount        string `json:"discAmount"`
 		MerchExtendParams string `json:"merchExtendParams"`
+		AuthWay           string `json:"authWay"`
+		PasswordURL       string `json:"passwordURL"`
 	} `json:"body"`
 }
 
