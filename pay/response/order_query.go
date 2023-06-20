3b375e0de5b722c5e77253fdd373e5095ccd6691
mahongran <623893204@qq.com>
2023-06-20 12:10:18 +0800

    fix

--- pay/response/order_query.go
+++ pay/response/order_query.go
@@ -5,20 +5,29 @@ import "encoding/json"
 type OrderQueryResponse struct {
 	Head header `json:"head"`
 	Body struct {
-		OriOrderCode   string `json:"oriOrderCode"`
-		OriRespCode    string `json:"oriRespCode"`
-		OriRespMsg     string `json:"oriRespMsg"`
-		TotalAmount    string `json:"totalAmount"`
-		OrderStatus    string `json:"orderStatus"`
-		BuyerPayAmount string `json:"buyerPayAmount"`
-		DiscAmount     string `json:"discAmount"`
-		PayTime        string `json:"payTime"`
-		ClearDate      string `json:"clearDate"`
-		MidFee         string `json:"midFee"`
-		PlMidFee       string `json:"plMidFee"`
-		SpecialFee     string `json:"specialFee"`
-		ExtraFee       string `json:"extraFee"`
-		Extend         string `json:"extend"`
+		OriOrderCode        string `json:"oriOrderCode"`
+		OriRespCode         string `json:"oriRespCode"`
+		OriRespMsg          string `json:"oriRespMsg"`
+		TotalAmount         string `json:"totalAmount"`
+		OrderStatus         string `json:"orderStatus"`
+		BuyerPayAmount      string `json:"buyerPayAmount"`
+		DiscAmount          string `json:"discAmount"`
+		PayTime             string `json:"payTime"`
+		ClearDate           string `json:"clearDate"`
+		MidFee              string `json:"midFee"`
+		PlMidFee            string `json:"plMidFee"`
+		SpecialFee          string `json:"specialFee"`
+		ExtraFee            string `json:"extraFee"`
+		Extend              string `json:"extend"`
+		OrderMsg            string `json:"orderMsg"`
+		ExternalProductCode string `json:"externalProductCode"`
+		MasterAccount       string `json:"masterAccount"`
+		SettleAmount        string `json:"settleAmount"`
+		AccNo               string `json:"accNo"`
+		OriTradeNo          string `json:"oriTradeNo"`
+		Bankserial          string `json:"bankserial"`
+		TxnCompleteTime     string `json:"txnCompleteTime"`
+		PayDetail           string `json:"payDetail"`
 	} `json:"body"`
 }
 
